1fcf41b08a72ffadf33389bb726e1d22d58786b5
swatakit <swatakit@gmail.com>
2024-02-12 22:48:34 +0800

    change from st.secret to os.environ for azure deployment

--- graph.py
+++ graph.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from langchain_community.graphs import Neo4jGraph
+import os
 
 graph = Neo4jGraph(
-    url=st.secrets["NEO4J_URI"],
-    username=st.secrets["NEO4J_USERNAME"],
-    password=st.secrets["NEO4J_PASSWORD"],
+    url=os.environ["NEO4J_URI"],
+    username=os.environ["NEO4J_USERNAME"],
+    password=os.environ["NEO4J_PASSWORD"],
 )
--- llm.py
+++ llm.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
+import os
 
 llm = ChatOpenAI(
-    openai_api_key=st.secrets["OPENAI_API_KEY"],
-    model=st.secrets["OPENAI_MODEL"],
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model=os.environ["OPENAI_MODEL"],
     temperature=0.0
 )
 
 from langchain_openai import OpenAIEmbeddings
 
 embeddings = OpenAIEmbeddings(
-    openai_api_key=st.secrets["OPENAI_API_KEY"]
+    openai_api_key=os.environ["OPENAI_API_KEY"]
 )
